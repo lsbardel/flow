689cb77d40ae96da64fe68df4078a2620e4f51ff
lsbardel <luca.sbardella@gmail.com>
2010-03-17 22:05:37 +0000

    more refactoring

--- src/jflow/db/report/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from models import *
-
-class LoggerAdmin(admin.ModelAdmin):
-    list_display = ('relevant_date', 'content_type', 'updated')
-admin.site.register(Logger,LoggerAdmin)
