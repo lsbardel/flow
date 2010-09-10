3129173d43fb8f507f4d4391df42482dccdae297
Quantmind <info@quantmind.com>
2010-09-10 11:43:56 +0100

    moved jfsite in the example directory

---
+++ src/examples/jfsite/cms/applications/forms.py
@@ -0,0 +1,85 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from djpcms.utils import form_kwargs
+from djpcms.utils.html import htmlwrap, box
+from djpcms.utils.uniforms import FormLayout, Html, Fieldset, inlineLabels, ModelFormInlineHelper
+from djpcms.forms import ModelMultipleChoiceField
+from djpcms.views.apps.tagging import TagField
+
+from flowrepo.models import Report, Attachment, Image
+from flowrepo.forms import FlowItemForm, add_related_upload
+from flowrepo import markups
+
+from jflow.db.instdata.models import DataId, EconometricAnalysis, VendorId
+from jflow.db.instdata.forms import DataIdForm, EconometricForm
+
+
+CRL_HELP = htmlwrap('div',
+                    htmlwrap('div',markups.help()).addClass('body').render()
+                   ).addClasses('flowitem report').render()
+
+
+collapse = lambda title, html, c, cl: box(hd = title, bd = html, collapsable = c, collapsed = cl)
+
+
+class NiceDataIdForm(DataIdForm):
+    tags   = TagField()
+    
+    layout = FormLayout()
+    layout.inlines.append(ModelFormInlineHelper(DataId,VendorId,extra=4))
+
+
+class InstrumentForm(forms.ModelForm):
+    pass
+
+
+class ReportForm(FlowItemForm):
+    authors  = ModelMultipleChoiceField(User.objects, required = False)
+    data_ids = ModelMultipleChoiceField(DataId.objects.all(),
+                                        required = False, label = 'Related securities')
+    attachments  = ModelMultipleChoiceField(Attachment.objects,
+                                            required = False,
+                                            label = 'Available attachments',
+                                            help_text = 'To select/deselect multiple files to attach press ctrl')
+    images       = ModelMultipleChoiceField(Image.objects, required = False, label = 'Available images',
+                                            help_text = 'To select/deselect multiple files to attach press ctrl')
+    tags         = TagField()
+    attachment_1 = forms.FileField(required = False)
+    attachment_2 = forms.FileField(required = False)
+    attachment_3 = forms.FileField(required = False)
+    
+    layout = FormLayout(
+                             Fieldset('name', 'description', 'body', key = 'body'),
+                    
+                             Fieldset('visibility', 'allow_comments',
+                                      css_class = inlineLabels),
+                            
+                             Fieldset('authors', 'tags', 'data_ids'),
+                             
+                             Html(CRL_HELP, key = 'help',
+                                      renderer = lambda html : collapse('Writing Tips',html,True,True)),
+                            
+                             Fieldset('attachments', 'images',
+                                      key = 'current_attachments',
+                                      renderer = lambda html : collapse('Attached files',html,True,False)),
+                             
+                             Fieldset('attachment_1', 'attachment_2', 'attachment_3',
+                                      key = 'attachments',
+                                      renderer = lambda html : collapse('New attachments',html,True,False)),
+                                      
+                             Fieldset('slug', 'timestamp', 'markup', 'parent',
+                                      key = 'metadata',
+                                      renderer = lambda html : collapse('Metadata',html,True,True)),
+                    
+                             template = 'flowrepo/report_form.html')
+    
+    def save(self, commit = True):
+        instance = super(ReportForm,self).save(commit = commit)
+        add_related_upload(self.cleaned_data['attachment_1'],instance)
+        add_related_upload(self.cleaned_data['attachment_2'],instance)
+        add_related_upload(self.cleaned_data['attachment_3'],instance)
+        return instance
+    
+    class Meta:
+        model = Report
---
+++ src/jflow/web/applications/data.py
@@ -0,0 +1,164 @@
+from django import http
+from django.contrib.contenttypes.models import ContentType
+from django.forms.models import modelform_factory
+
+from djpcms.conf import settings
+from djpcms.utils.ajax import jhtmls
+from djpcms.views import appsite, appview
+from djpcms.views.apps.tagging import TagApplication
+
+from dateutil.parser import parse as DateFromString
+from unuk.core.jsonrpc import Proxy
+
+from jflow.db.instdata.models import DataId, EconometricAnalysis, VendorId
+from jflow.jfsite.cms.applications.forms import NiceDataIdForm, ReportForm, EconometricForm, InstrumentForm
+
+
+def date2yyyymmdd(dte):
+    return dte.day + 100*(dte.month + 100*dte.year)
+
+
+def econometric_data(data,url):
+    cts    = data.get('command',None)
+    start  = data.get('start',None)
+    end    = data.get('end',None)
+    period = data.get('period',None)
+    if start:
+        start = date2yyyymmdd(DateFromString(str(start)).date())
+    if end:
+        end = date2yyyymmdd(DateFromString(str(end)).date())
+    proxy = Proxy(url)
+    try:
+        return proxy.raw_history(cts,start,end)
+    except IOError:
+        return ''
+
+
+def getserver():
+    #from jflow.db.netdata.models import ServerMachine
+    #return ServerMachine.objects.get_for_machine('jflow-data-server')
+    return 'http://localhost:9010/'
+
+class TimeserieView(appview.AppView):
+    '''
+    view used to obtain timeseries.
+    The only view available is an Ajax Get view
+    '''
+    _methods      = ('get',)
+    
+    def get_response(self, djp):
+        request = djp.request
+        if not request.is_ajax():
+            raise http.Http404
+        data = dict(request.GET.items())
+        server = getserver()
+        sdata = econometric_data(data,server)
+        return http.HttpResponse(sdata, mimetype='application/javascript')
+
+
+def change_type(self, djp):
+    '''Ajax view to change instrument form'''
+    form = self.get_form(djp, withdata = False)
+    forms = list(form.forms_only())
+    if len(forms) == 2:
+        form = forms[1]
+        html = form.layout.render(form)
+    else:
+        html = ''
+    return jhtmls(html = html, identifier = '.data-id-instrument')
+
+
+
+class DataAddView(appview.AddView):
+
+    def ajax__content_type(self, djp):
+        return change_type(self,djp)
+
+class DataEditView(appview.EditView):
+
+    def ajax__content_type(self, djp):
+        return change_type(self,djp)
+    
+    def title(self, page, **urlargs):
+        return u'edit'
+
+
+slug_regex = '(?P<id>[-\.\w]+)'
+
+class DataApplication(TagApplication):
+    inherit   = True
+    form      = NiceDataIdForm
+    form_template = 'instdata/dataid_change_form.html'
+    search_fields = ['code','name','description','tags','isin']
+    
+    timeserie = TimeserieView(regex = 'timeseries')
+    complete  = appview.AutocompleteView()
+    add       = DataAddView(regex = 'add', isplugin = False)
+    view      = appview.ViewView(regex = slug_regex, parent = None)
+    edit      = DataEditView()
+    
+    def objectbits(self, obj):
+        return {'id': obj.code}
+    
+    def get_object(self, *args, **kwargs):
+        try:
+            id = kwargs.get('id',None)
+            return self.model.objects.get(code = id)
+        except:
+            return None
+    
+    def instrument_form(self, request, instance, bound):
+        data     = request.POST or request.GET
+        initial  = dict(data.items())
+        ct = initial.get('content_type',None)
+        instrument = None
+        imodel     = None
+        if not ct and instance:
+            instrument = instance.instrument
+            if instrument:
+                imodel = instrument.__class__
+        if not imodel and ct:
+            try:
+                ct = ContentType.objects.get(id = ct)
+                imodel = ct.model_class()
+            except:
+                pass
+        if imodel:
+            f = modelform_factory(imodel, InstrumentForm)
+            if bound:
+                return f(data = data, instance = instrument)
+            else:
+                return f(initial = initial, instance = instrument)
+        else:
+            return None
+        
+    def get_form(self, djp, withdata = True, initial = None, **kwargs):
+        initial = initial or {}
+        if not withdata:
+            initial.update(dict(djp.request.POST.items()))
+        f = super(DataApplication,self).get_form(djp, initial = initial, withdata = withdata, **kwargs)
+        dataform = f.forms[0][1]
+        iform    = dataform.content_form
+        if iform:
+            f.add(iform)
+        return f
+    
+    def object_from_form(self, form):
+        form.forms.pop()
+        return super(DataApplication,self).object_from_form(form)
+    
+
+class EconometricApplication(TagApplication):
+    inherit = True
+    form = EconometricForm
+    form_withrequest = True
+    
+    add     = appview.AddView(regex = 'add')
+    #edit    = appview.EditView(regex = 'edit/(?P<id>\d+)', parent = None)
+    view      = appview.ViewView(regex = '(?P<id>[-\.\w]+)')
+    
+
+#_______________________________________________________________________ MANUAL TRADE
+class ManualTradeApplication(appsite.ModelApplication):
+    search = appview.SearchView()
+    add = appview.AddView()
